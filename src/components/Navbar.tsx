c786531d0cfa83204ea1a3721703d20a7c9099c5
tcgpokemarket <tcgpokemarketadmin@gmail.com>
2026-07-06 15:42:33 -0700

    Add TCG navigation component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,2 @@
+import Link from 'next/link'
+export default function Navbar(){return <nav><Link href='/'>TCG Poke Market</Link> | <Link href='/listings'>Marketplace</Link> | <Link href='/sell'>Sell</Link> | <Link href='/dashboard'>Dashboard</Link> | <Link href='/auth'>Login</Link></nav>}
