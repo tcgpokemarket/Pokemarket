823e1e8e65a553dd268e39af7c21fa322c744d86
Trent <tcgpokemarketadmin@gmail.com>
2026-07-07 19:55:45 +0000

    Update homepage and export config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  },
 };
 
 module.exports = nextConfig;
